88325e6857d01de25e8e9005dd72fa94bb9b2fcb
crudajunluiz <crudajunluiz@gmail.com>
2026-09-07 15:34:55 +0800

    Update even_odd_detector.py

--- even_odd_detector.py
+++ even_odd_detector.py
@@ -1,5 +1,8 @@
-num1 = int(input("Enter a number: "))
-if num1 % 2 == 0:
-  print(f"{num1} is an even number")
-else:
-  print(f"{num1} is an odd number")
+# Even or Odd Detector
+
+num1 = int(input("Enter a number: ")) # Asks for an input to detect if it is an odd or even number.
+
+if num1 % 2 == 0: # Checks if it is even
+  print(f"{num1} is an even number") # Inputs the message if the number was even
+else: # Checks if it is not even
+  print(f"{num1} is an odd number") # Inputs the message if the number was odd
